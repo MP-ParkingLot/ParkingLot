2565c46f3e659ea8e8bfcbf969fafb2641fcbaf5
chohs4164 <josh100as@gmail.com>
2025-05-25 13:30:50 +0900

    feat: 주차장 UI 상태 관리용 데이터 클래스 추가

--- app/src/main/java/com/example/parkinglot/uistate/ParkingLotUiState.kt
+++ app/src/main/java/com/example/parkinglot/uistate/ParkingLotUiState.kt
@@ -3,6 +3,8 @@ package com.example.parkinglot.uistate
 import com.example.parkinglot.dto.response.ParkingLotDetail
 
 data class ParkingLotUiState(
+    var showOnlyAvailable: Boolean = false, //빈자리 필터
+    var showOnlyFree: Boolean = false, // 무료 필터
     val isLoading: Boolean = false,
     val error: String? = null,
     val parkingLots: List<Pair<String, ParkingLotDetail>> = emptyList()
