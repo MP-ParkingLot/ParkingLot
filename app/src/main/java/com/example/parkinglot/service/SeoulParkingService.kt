240d2ef1554e3157279aed6c23831632436365f5
chohs4164 <josh100as@gmail.com>
2025-05-25 13:31:29 +0900

    feat: 주차장 필터링 관련 Retrofit API 인터페이스 정의

--- app/src/main/java/com/example/parkinglot/service/SeoulParkingService.kt
+++ app/src/main/java/com/example/parkinglot/service/SeoulParkingService.kt
@@ -1,13 +1,34 @@
 package com.example.parkinglot.service
 
 import com.example.parkinglot.dto.request.NearByParkinglotRequest
+import com.example.parkinglot.dto.response.EmptyParkinglotResponse
+import com.example.parkinglot.dto.response.FreeParkinglotResponse
 import com.example.parkinglot.dto.response.NearByParkinglotResponse
+import com.example.parkinglot.dto.response.RegionParkinglotResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface SeoulParkingService {
-    @POST("/parkinglot")
+    @POST("/parkinglot") //주차장 정보 조회
     suspend fun getNearbyParkingLots(
         @Body request: NearByParkinglotRequest
     ): NearByParkinglotResponse
+
+    @GET("/parkinglot") //행정 구역 별
+    suspend fun getRegionParkingLots(
+        @Query("district") district: String
+    ): RegionParkinglotResponse
+
+    @POST("/parkinglot/empty") //빈자리 필터
+    suspend fun getEmptyParkingLots(
+        @Body request: NearByParkinglotRequest
+    ): EmptyParkinglotResponse
+
+    @POST("/parkinglot/free") //무료 필터
+    suspend fun getFreeParkingLots(
+        @Body request: NearByParkinglotRequest
+    ): FreeParkinglotResponse
+
 }
