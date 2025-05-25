95e4458ecdabf67e621f6f1d0b0db32efef11a60
chohs4164 <josh100as@gmail.com>
2025-05-25 13:29:30 +0900

    feat: 빈자리 주차장 응답 DTO 정의

---
+++ app/src/main/java/com/example/parkinglot/dto/response/EmptyParkinglotResponse.kt
@@ -0,0 +1,11 @@
+package com.example.parkinglot.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class EmptyParkinglotResponse(
+    @SerialName("parkingLot")
+    val parkingLot: Map<String, ParkingLotDetail>
+)
+
