d65a5aa0e8496ed78bd214edc7ec3c09d47835f5
chohs4164 <josh100as@gmail.com>
2025-05-25 13:30:58 +0900

    feat: 주차장 필터링 API 호출 함수 추가 (빈자리/무료/행정구)

--- app/src/main/java/com/example/parkinglot/repository/ParkingRepository.kt
+++ app/src/main/java/com/example/parkinglot/repository/ParkingRepository.kt
@@ -1,8 +1,28 @@
 package com.example.parkinglot.repository
 
 import com.example.parkinglot.dto.request.NearByParkinglotRequest
+import com.example.parkinglot.dto.response.EmptyParkinglotResponse
+import com.example.parkinglot.dto.response.FreeParkinglotResponse
+import com.example.parkinglot.dto.response.NearByParkinglotResponse
+import com.example.parkinglot.dto.response.RegionParkinglotResponse
 
 class ParkingRepository {
+    //주차장 정보 조회
     suspend fun getNearbyParkingLots(request: NearByParkinglotRequest) =
         RetrofitClient.seoulParkingService.getNearbyParkingLots(request)
+
+    //빈자리 필터
+    suspend fun getEmptyParkingLots(request: NearByParkinglotRequest): EmptyParkinglotResponse {
+        return RetrofitClient.seoulParkingService.getEmptyParkingLots(request)
+    }
+
+    //무료 필터
+    suspend fun getFreeParkingLots(request: NearByParkinglotRequest): FreeParkinglotResponse {
+        return RetrofitClient.seoulParkingService.getFreeParkingLots(request)
+    }
+
+    //행정 구역 별
+    suspend fun getRegionParkingLots(district: String): RegionParkinglotResponse {
+        return RetrofitClient.seoulParkingService.getRegionParkingLots(district)
+    }
 }
