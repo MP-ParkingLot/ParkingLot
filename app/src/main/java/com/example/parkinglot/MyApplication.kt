e71662ca87b69f3127fa3777475ba92bb2d5f334
chohs4164 <josh100as@gmail.com>
2025-06-11 00:35:23 +0900

    feat : 앱 진입점 및 초기 설정 및 상태 관리

--- app/src/main/java/com/example/parkinglot/MyApplication.kt
+++ app/src/main/java/com/example/parkinglot/MyApplication.kt
@@ -1,11 +1,14 @@
+//app/src/main/java/com/example/parkinglot/MyApplication.kt
 package com.example.parkinglot
 
 import android.app.Application
-import com.kakao.vectormap.KakaoMapSdk
+import com.kakao.vectormap.KakaoMapSdk // Kakao Vector Map SDK 임포트
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        KakaoMapSdk.init(this, BuildConfig.KAKAO_REST_KEY)
+        // Kakao Vector Map SDK 초기화
+        // BuildConfig.KAKAO_REST_KEY는 secrets-gradle-plugin이 secrets.properties에서 생성합니다.
+        KakaoMapSdk.init(this, BuildConfig.KAKAO_MAP_KEY)
     }
 }
